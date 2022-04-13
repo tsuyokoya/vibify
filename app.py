c30c7893966da0fbeaa1286b341d1e1d99fdbdd7
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-04-13 08:10:04 -0400

    update app.py config

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # Prints all SQL statements to terminal
 app.config["SQLALCHEMY_ECHO"] = True
 
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or "as89hw8h2fisdfh903"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "as89hw8h2fisdfh903")
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 debug = DebugToolbarExtension(app)
 
