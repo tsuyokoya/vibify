781fa37d2486be30a0320c7500ffa4d0849cbbc7
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-04-13 08:03:51 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+"""Vibify application."""
+
+from flask import Flask, render_template, redirect, request
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+import os
+import re
+
+# from models import
+
+app = Flask(__name__)
+
+
+uri = os.environ.get("DATABASE_URL", "postgresql:///vibify")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+# Specify the database
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
+# Default is True, turn off to prevent overhead
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Prints all SQL statements to terminal
+app.config["SQLALCHEMY_ECHO"] = True
+
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or "as89hw8h2fisdfh903"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
+
+
+@app.route("/")
+def show_home_page():
+    """Renders home page"""
+
+    return render_template("base.html")
