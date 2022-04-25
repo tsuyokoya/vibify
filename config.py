6c90de99157b0a15227a540cb5548650c9b1205b
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-04-25 13:41:50 -0400

    flask config update

---
+++ config.py
@@ -0,0 +1,28 @@
+from base64 import b64encode
+import os
+
+uri = os.environ.get("DATABASE_URL", "postgresql:///vibify")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get("SECRET_KEY", b64encode(os.urandom(64)).decode("utf-8"))
+
+
+class ProductionConfig(Config):
+    ENV = "production"
+    SQLALCHEMY_DATABASE_URI = uri
+
+
+class DevelopmentConfig(Config):
+    ENV = "development"
+    SESSION_COOKIE_SECURE = False
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+    SQLALCHEMY_DATABASE_URI = uri
+
+
+class TestingConfig(Config):
+    TESTING = True
