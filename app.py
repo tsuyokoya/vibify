02d629acf67c0d4560a3dd40d245e7a1975cc0ca
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-05-31 18:35:18 -0400

    implemented SpotifyAPI class

--- app.py
+++ app.py
@@ -2,13 +2,13 @@
 
 import base64
 import requests
-import spotify
 import urllib
 from flask import Flask, render_template, redirect, request, session, g, make_response
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_migrate import Migrate
 from models import db, connect_db, User, Playlist, Song, Playlist_Song
 from forms import CreatePlaylistForm, LoginForm, RegistrationForm
+from spotify import SpotifyAPI
 
 app = Flask(__name__)
 
@@ -20,6 +20,8 @@ else:
 debug = DebugToolbarExtension(app)
 migrate = Migrate(app, db)
 connect_db(app)
+
+spotify = SpotifyAPI()
 
 
 @app.route("/home")
@@ -80,31 +82,10 @@ def register_with_spotify():
     client_id = app.config["CLIENT_ID"]
     redirect_uri = app.config["REDIRECT_URI"]
     scope = app.config["SCOPE"]
-    # print(client_id, redirect_uri, scope)
-    # need to update state_key
-    # state_key = "1234567890qwert"
-    # session["state_key"] = state_key
 
-    # authorize_base_url = "https://accounts.spotify.com/en/authorize?"
-    # query = {
-    #     "response_type": "code",
-    #     "client_id": client_id,
-    #     "redirect_uri": redirect_uri,
-    #     "scope": scope,
-    #     "state": state_key,
-    # }
-    # print(authorize_base_url + urllib.parse.urlencode(query))
-    # authorize_url = authorize_base_url + urllib.parse.urlencode(query)
     authorize_url = spotify.authorize_user(client_id, redirect_uri, scope)
-    print("************", authorize_url)
 
     return redirect(authorize_url)
-
-    # make response
-    # use to add more headers / edit the response
-    # takes input and uses it in body of response -> resulting body allows you to customize response
-    # basically what flask does internally
-    # my_resp.headers[''] = ''
 
 
 @app.route("/callback")
@@ -123,26 +104,10 @@ def callback():
         client_creds = f"{client_id}:{client_secret}"
         token_url = "https://accounts.spotify.com/api/token"
 
-        # token_headers = {
-        #     "Authorization": f"Basic {client_creds_b64.decode()}",
-        #     "Accept": "application/json",
-        #     "Content-Type": "application/x-www-form-urlencoded",
-        # }
-        # token_data = {
-        #     "code": code,
-        #     "redirect_uri": redirect_uri,
-        #     "grant_type": "authorization_code",
-        # }
         token_headers = spotify.get_token_headers(client_creds)
         token_data = spotify.get_token_data(code, redirect_uri)
         post_response = requests.post(token_url, headers=token_headers, data=token_data)
-        print("THIS IS THE POST RESPONSE", post_response.json())
 
-        # 200 code indicates access token was properly granted
         if post_response.status_code == 200:
             json = post_response.json()
-            print("THIS IS THE JSON", json)
-            # return json["access_token"], json["refresh_token"], json["expires_in"]
             return redirect("/")
-        # else:
-        #     return redirect("/404")
--- spotify.py
+++ spotify.py
@@ -5,50 +5,45 @@ import urllib
 from flask import session
 
 
-def create_state_key():
-    characters = string.ascii_letters + string.digits
-    return "".join(random.choice(characters) for i in range(15))
+class SpotifyAPI:
+    def create_state_key(self):
+        characters = string.ascii_letters + string.digits
+        return "".join(random.choice(characters) for i in range(15))
 
+    def base64_encode(self, client_credentials):
+        return base64.b64encode(client_credentials.encode())
 
-def base64_encode(client_credentials):
-    return base64.b64encode(client_credentials.encode())
+    def authorize_user(self, client_id, redirect_uri, scope):
+        """Authorizes the use of user Spotify account data"""
+        state_key = self.create_state_key()
 
+        session["state_key"] = state_key
 
-def authorize_user(client_id, redirect_uri, scope):
-    """Authorizes the use of user Spotify account data"""
-    state_key = create_state_key()
+        authorize_base_url = "https://accounts.spotify.com/en/authorize?"
+        query = {
+            "response_type": "code",
+            "client_id": client_id,
+            "redirect_uri": redirect_uri,
+            "scope": scope,
+            "state": state_key,
+        }
+        authorize_url = authorize_base_url + urllib.parse.urlencode(query)
 
-    session["state_key"] = state_key
+        return authorize_url
 
-    authorize_base_url = "https://accounts.spotify.com/en/authorize?"
-    query = {
-        "response_type": "code",
-        "client_id": client_id,
-        "redirect_uri": redirect_uri,
-        "scope": scope,
-        "state": state_key,
-    }
-    authorize_url = authorize_base_url + urllib.parse.urlencode(query)
+    def get_token_headers(self, client_creds):
+        client_creds_b64 = self.base64_encode(client_creds)
+        return {
+            "Authorization": f"Basic {client_creds_b64.decode()}",
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
 
-    return authorize_url
+    def get_token_data(self, code, redirect_uri):
+        return {
+            "code": code,
+            "redirect_uri": redirect_uri,
+            "grant_type": "authorization_code",
+        }
 
-
-def get_token_headers(client_creds):
-
-    client_creds_b64 = base64_encode(client_creds)
-    return {
-        "Authorization": f"Basic {client_creds_b64.decode()}",
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
-
-
-def get_token_data(code, redirect_uri):
-    return {
-        "code": code,
-        "redirect_uri": redirect_uri,
-        "grant_type": "authorization_code",
-    }
-
-
-def get_refreshed_token(refresh_token):
-    return {"grant_type": "refresh_token", "refresh_token": refresh_token}
+    def get_refreshed_token(self, refresh_token):
+        return {"grant_type": "refresh_token", "refresh_token": refresh_token}
