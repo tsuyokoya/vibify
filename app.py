37f9587c58110e118d6c5ac8abc4239dd6b86c97
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-06-08 11:26:08 -0400

    Refactored spotify authentication and added homepage form validation

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from flask_migrate import Migrate
 from models import db, connect_db, User, Playlist, Song, Playlist_Song
 from forms import CreatePlaylistForm, LoginForm, RegistrationForm
 from spotify import SpotifyAPI
-from helpers import get_user_spotify_data
 
 app = Flask(__name__)
 
@@ -28,16 +27,28 @@ logging.basicConfig(
     format="%(asctime)s %(levelname)s %(name)s : %(message)s",
 )
 
-token_url = "https://accounts.spotify.com/api/token"
+client_id = app.config["CLIENT_ID"]
+client_secret = app.config["CLIENT_SECRET"]
+redirect_uri = app.config["REDIRECT_URI"]
+scope = app.config["SCOPE"]
+spotify = SpotifyAPI(client_id, client_secret, redirect_uri, scope)
 
-spotify = SpotifyAPI()
 
-
-@app.route("/home")
-@app.route("/")
+@app.route("/home", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def show_home_page():
-    """Renders home page"""
+    """Renders home page / Creates playlist"""
     form = CreatePlaylistForm()
+
+    if form.validate_on_submit():
+        user_id = g.user.id or "guest"
+        Playlist.create(form.title.data, form.vibe.data, user_id)
+        playlist = spotify.create_user_playlist()
+        app.logger.info(playlist)
+
+        flash("Successfully created playlist", "success")
+        return redirect("/playlists")
+
     return render_template("base.html", form=form)
 
 
@@ -80,7 +91,7 @@ def show_login_page():
             flash("Successfully logged in", "success")
             return redirect("/")
 
-        flash("Invalid email / password", "warning")
+        flash("Invalid email / password", "danger")
 
     return render_template("login.html", form=form)
 
@@ -88,7 +99,7 @@ def show_login_page():
 @app.route("/logout")
 def logout_user():
     do_logout()
-    flash("Logged out", "warning")
+    flash("Logged out", "danger")
     return redirect("/")
 
 
@@ -105,18 +116,14 @@ def show_register_page():
             flash("Successfully logged in", "success")
             return redirect("/")
 
-        flash("Invalid email / password", "warning")
+        flash("Invalid email / password", "danger")
     return render_template("register.html", form=form)
 
 
 @app.route("/authorize")
 def register_with_spotify():
     """Authorizes use of user Spotify account data"""
-    client_id = app.config["CLIENT_ID"]
-    redirect_uri = app.config["REDIRECT_URI"]
-    scope = app.config["SCOPE"]
-
-    authorize_url = spotify.authorize_user(client_id, redirect_uri, scope)
+    authorize_url = spotify.get_authorization_url()
     app.logger.info(authorize_url)
     return redirect(authorize_url)
 
@@ -130,26 +137,14 @@ def callback():
 
     if state != session["state_key"]:
         return False
-    else:
-        redirect_uri = app.config["REDIRECT_URI"]
-        client_secret = app.config["CLIENT_SECRET"]
-        client_id = app.config["CLIENT_ID"]
-        client_creds = f"{client_id}:{client_secret}"
-
-        token_headers = spotify.get_token_headers(client_creds)
-        token_data = spotify.get_token_data(code, redirect_uri)
-        post_response = requests.post(token_url, headers=token_headers, data=token_data)
-
-        if post_response.status_code == 200:
-            json = post_response.json()
-            user_data = get_user_spotify_data(json)
-            user = User.register(
-                user_data["display_name"], user_data["email"], "password"
-            )
-            do_login(user)
-            return redirect("/")
+    is_authorized = spotify.authorize_user(code)
+    if is_authorized:
+        user_data = spotify.get_user_spotify_data()
+        user = User.register(user_data["display_name"], user_data["email"], "password")
+        do_login(user)
+        return redirect("/")
 
 
-@app.route("/playlists")
+@app.route("/playlists", methods=["GET", "POST"])
 def show_playlists_page():
     return render_template("playlists.html")
