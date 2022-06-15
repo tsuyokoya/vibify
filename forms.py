33fdb402b2cebe49e30c3694169a98580d1d81c3
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-06-15 11:46:00 -0400

    Update title validator and vibe range

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms import (
     StringField,
     DecimalRangeField,
 )
-from wtforms.validators import InputRequired, NumberRange
+from wtforms.validators import NumberRange, Length
 
 
 class CreatePlaylistForm(FlaskForm):
@@ -11,7 +11,7 @@ class CreatePlaylistForm(FlaskForm):
 
     title = StringField(
         "Playlist Title",
-        validators=[InputRequired()],
         render_kw={"placeholder": "Enter playlist title"},
+        validators=[Length(max=20)],
     )
-    vibe = DecimalRangeField(validators=[NumberRange(min=0, max=10)])
+    vibe = DecimalRangeField(validators=[NumberRange(min=0, max=1)])
