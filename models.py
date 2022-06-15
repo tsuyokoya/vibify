ee144aa1bdb5672ccef240083c31118577d1c438
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-06-15 11:47:35 -0400

    Updated Song model and added class methods

--- models.py
+++ models.py
@@ -75,7 +75,7 @@ class Playlist(db.Model):
     __tablename__ = "playlists"
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    name = db.Column(db.String(25), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
     user_id = db.Column(
         UUID(as_uuid=True), db.ForeignKey("users.id", ondelete="cascade")
     )
@@ -108,12 +108,26 @@ class Song(db.Model):
 
     __tablename__ = "songs"
 
-    uri = db.Column(db.String, primary_key=True)
-    title = db.Column(db.String(50), nullable=False)
-    artist = db.Column(db.String(50), nullable=False)
-    album = db.Column(db.String(50))
-    artwork = db.Column(db.Text)
-    preview_url = db.Column(db.Text)
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    preview_url = db.Column(db.String)
+    artist = db.Column(db.String, nullable=False)
+    album_name = db.Column(db.String)
+    album_image_url = db.Column(db.String)
+
+    @classmethod
+    def create(cls, id, name, preview_url, artist, album_name, album_image_url):
+        song = Song(
+            id=id,
+            name=name,
+            preview_url=preview_url,
+            artist=artist,
+            album_name=album_name,
+            album_image_url=album_image_url,
+        )
+        db.session.add(song)
+        db.session.commit()
+        return song
 
 
 # - playlists_songs
@@ -133,7 +147,14 @@ class Playlist_Song(db.Model):
     )
 
     song_id = db.Column(
-        UUID(as_uuid=True),
+        db.String,
         db.ForeignKey("songs.id", ondelete="cascade"),
         primary_key=True,
     )
+
+    @classmethod
+    def create(cls, playlist_id, song_id):
+        playlist_song = Playlist_Song(playlist_id=playlist_id, song_id=song_id)
+        db.session.add(playlist_song)
+        db.session.commit()
+        return playlist_song
