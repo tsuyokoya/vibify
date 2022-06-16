b06dd382f73639ff6b510e3f91eb6353973d0557
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-06-16 08:32:28 -0400

    Updated User and Song models

--- models.py
+++ models.py
@@ -27,14 +27,16 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    spotify_id = db.Column(db.String, nullable=False)
     name = db.Column(db.String(25), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
 
     @classmethod
-    def register(cls, name, email):
+    def register(cls, spotify_id, name, email):
         """Register user. Password is hashed. Adds user to system."""
 
         user = User(
+            spotify_id=spotify_id,
             name=name,
             email=email,
         )
@@ -111,15 +113,17 @@ class Song(db.Model):
     id = db.Column(db.String, primary_key=True)
     name = db.Column(db.String, nullable=False)
     valence = db.Column(db.Numeric(3, 2))
+    uri = db.Column(db.String, nullable=False)
     artist = db.Column(db.String, nullable=False)
     album_name = db.Column(db.String)
     album_image_url = db.Column(db.String)
 
     @classmethod
-    def create(cls, id, name, artist, album_name, album_image_url):
+    def create(cls, id, name, uri, artist, album_name, album_image_url):
         song = Song(
             id=id,
             name=name,
+            uri=uri,
             artist=artist,
             album_name=album_name,
             album_image_url=album_image_url,
