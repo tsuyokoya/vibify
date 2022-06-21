ad4aa368187169d7f50044c735c15d62f17db464
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-06-21 10:10:00 -0400

    Added testing config option

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ app = Flask(__name__)
 
 if app.config["ENV"] == "production":
     app.config.from_object("config.ProductionConfig")
+elif app.config["ENV"] == "testing":
+    app.config.from_object("config.TestingConfig")
 else:
     app.config.from_object("config.DevelopmentConfig")
 
