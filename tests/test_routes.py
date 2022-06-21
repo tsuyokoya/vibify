959f39769dbe101fd14b81b299ec96c3c67a28f0
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-06-21 10:12:01 -0400

    Updated routes test case

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,35 +1,25 @@
 """Vibify routes tests."""
+import sys
 
+sys.path.append("../")
 from unittest import TestCase
-
-# from models import db, User, Playlist, Song, Playlist_Song
-# import os
-# os.environ["DATABASE_URL"] = "postgresql:///vibify"
-
 from app import app
-
-# db.create_all()
 
 
 class RoutesTestCase(TestCase):
     """Test routes."""
 
+    def setUp(self):
+        """Create test client"""
+        self.client = app.test_client()
+
     def test_home_page(self):
         """Does home page render?"""
 
-        with app.test_client() as client:
-            res = client.get("/")
+        with self.client as c:
+            res = c.get("/")
             html = res.get_data(as_text=True)
 
             self.assertEqual(res.status_code, 200)
-            self.assertIn("Generate Spotify playlists based on your vibe.", html)
-
-    def test_login_page(self):
-        """Does login page render?"""
-
-        with app.test_client() as client:
-            res = client.get("/login")
-            html = res.get_data(as_text=True)
-
-            self.assertEqual(res.status_code, 200)
-            self.assertIn("New to Vibify? Sign Up", html)
+            self.assertIn("Generate Spotify playlists based on your vibe", html)
+            self.assertIn("Sign In", html)
